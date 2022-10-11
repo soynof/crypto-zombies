604bc19b632223a19e8cedd0731e945a4db9c7ff
soynof <87549285+soynof@users.noreply.github.com>
2022-10-10 23:28:33 -0300

    Re-factor

--- zombie-factory.sol
+++ zombie-factory.sol
@@ -12,11 +12,15 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string memory _name, uint _dna) private {
+    function _createZombie(string memory _name, uint _dna) internal {
         Zombie memory zombie = Zombie(_name, _dna);
         zombies.push(zombie);
         uint id = zombies.length - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
@@ -26,6 +30,7 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(bytes32 _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         string memory name = string(abi.encodePacked(_name));
         uint randDna = _generateRandomDna(name);
         _createZombie(name, randDna); 
